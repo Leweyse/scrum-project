d396ae59bd98493ef5637e711341892a429d864d
Sushanta Pyakurel <pyakurelsushanta@gmail.com>
2021-12-21 10:39:37 +0100

    Migrtion files added

---
+++ api/database/migrations/2021_12_21_084312_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('categories_id');
+            $table->string('sku');
+            $table->string('title');
+            $table->text('description');
+            $table->string('image');
+            $table->integer('price');
+            $table->integer('stock_unit');
+            $table->integer('sold_unit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ api/database/migrations/2021_12_21_084318_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('users_id');
+            $table->string('user_ip');
+            $table->integer('sub_total');
+            $table->string('postcode');
+            $table->string('city');
+            $table->string('address_line_1');
+            $table->string('address_line_2')->nullable();
+            $table->string('country');
+            $table->string('status')->default('new');
+            $table->date('shipped_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
