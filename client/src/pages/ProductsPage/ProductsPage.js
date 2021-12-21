4d12b787ac83ed6279503233bf3dafe649bc17e4
Leweyse <darylcastro67@gmail.com>
2021-12-21 15:11:29 +0100

    implemented new component

--- client/src/pages/ProductsPage/ProductsPage.js
+++ client/src/pages/ProductsPage/ProductsPage.js
@@ -1,15 +1,12 @@
-import { ProductCard } from '../../components';
+import { StockSection } from '../../components';
 
 const ProductsPage = () => {
     return (
-        <main id={'productsPage'}>
-            <ProductCard 
-                src={'https://m.media-amazon.com/images/I/61kocbtP2QL._AC_SL1000_.jpg'}
-                title={'Vinyl Ex:Re'}
-                price={'30.00 $'}
-                seller={'Daryl Castro'}
-            />
-        </main>
+        <>
+            {/* <NavBar /> */}
+            <StockSection />
+            {/* <Footer /> */}
+        </>
     )
 }
 
