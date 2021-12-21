87b5a1992750a66e5ecc47337396e58e5ccd6160
Katya Heylen <ezhgurova@gmail.com>
2021-12-21 16:41:46 +0100

    landingpage and slideshow element added

---
+++ client/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import Navbar from Navbar;
+import Slideshow from ./container/Slideshow;
+
+export default function LandingPage () {
+    return (
+        <Navbar></Navbar>
+        <h1>B-Bay</h1>
+        <Slideshow></Slideshow>
+        <footer></footer>
+    )
+}
