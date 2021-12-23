33d21704e7483fead253ffe8e2e1eed773cbfb29
Sushanta Pyakurel <pyakurelsushanta@gmail.com>
2021-12-23 10:33:29 +0100

    Testing the Login Form building

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
     return (
         <>
             <LoginPage/>
-            <ProductsPage/>
+            {/*<ProductsPage/>*/}
         </>
     );
 }
--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,19 +1,98 @@
-export default function LoginPage () {
+import {useState, useRef} from 'react';
+
+// class LoginPage extends  React.Component {
+//
+//     constructor(props) {
+//         this.state = {
+//             email: '',
+//             password: ''
+//         };
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+//
+//     handleChange(event) {
+//         this.setState({value: event.target.value});
+//     }
+//
+//     handleSubmit(event) {
+//         alert('Email' + this.state.email);
+//         event.preventDefault();
+//     }
+//
+//     render() {
+//     return (
+//         <div id={"loginPage"}>
+//             <div id={"loginContainer"}>
+//                 <p id={"loginLogo"}>B-bay</p>
+//                 <p id={"loginLogoCropped"}>B-bay</p>
+//                 <form id={"loginForm"} onSubmit={this.handleSubmit}>
+//                     <input
+//                         id={"loginEmail"}
+//                         value={this.state.email}
+//                         onChange={this.handleChange}
+//                         placeholder={"E-mail address"}
+//                     />
+//                     <input
+//                         id={"loginPassword"}
+//                         value={this.state.password}
+//                         onChange={this.handleChange}
+//                         placeholder={"Password"}
+//                     />
+//                     <div id={"loginFormButtonContainer"}>
+//                         <button id={"signUp"}>Sign up</button>
+//                         <button id={"loginSubmit"}>Submit</button>
+//                     </div>
+//                 </form>
+//                 <a id={"passwordReset"}>Forgot password?</a>
+//             </div>
+//         </div>
+//     )
+//     }
+// }
+//
+// export default LoginPage;
+
+const LoginPage = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const inputRefEmail = useRef();
+    const inputRefPassword = useRef();
+
+    const handleSubmit = (event) => {
+        console.log(inputRefEmail.current);
+        alert('Email : ' + email);
+        event.preventDefault();
+    }
+    // const handleChange = (input) => {
+    //     setEmail(input);
+    // }
+
     return (
         <div id={"loginPage"}>
-            <div id={"loginContainer"}>
-                <p id={"loginLogo"}>B-bay</p>
-                <p id={"loginLogoCropped"}>B-bay</p>
-                <form id={"loginForm"}>
-                    <input id={"loginEmail"} type={"email"} placeholder={"E-mail address"}/>
-                    <input id={"loginPassword"} type={"password"} placeholder={"Password"}/>
-                    <div id={"loginFormButtonContainer"}>
-                        <button id={"signUp"}>Sign up</button>
-                        <button id={"loginSubmit"}>Submit</button>
+                       <div id={"loginContainer"}>
+                             <p id={"loginLogo"}>B-bay</p>
+                              <p id={"loginLogoCropped"}>B-bay</p>
+                             <form id={"loginForm"} onSubmit={handleSubmit}>
+                                 <input
+                                    ref={inputRefEmail}
+                                    id={"loginEmail"}
+                                    placeholder={"E-mail address"}
+                                />
+                                <input
+                                    ref={inputRefPassword}
+                                    id={"loginPassword"}
+                                    placeholder={"Password"}
+                                />
+                                <div id={"loginFormButtonContainer"}>
+                                    <button id={"signUp"}>Sign up</button>
+                                    <button id={"loginSubmit"}>Submit</button>
+                                </div>
+                            </form>
+                            <a id={"passwordReset"}>Forgot password?</a>
+                        </div>
                     </div>
-                </form>
-                <a id={"passwordReset"} href={""}>Forgot password?</a>
-            </div>
-        </div>
     )
 }
+
+export default LoginPage;
