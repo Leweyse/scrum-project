f49cf5cbebf8e5722fb848410586992e5be2536e
jvondzerza <jvondzerza@googlemail.com>
2021-12-23 11:12:21 +0100

    built product page and implemented preliminary styling, need to add data graph for price statistics in place of placeholder div

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,11 @@
 import LandingPage from './LandingPage/LandingPage';
 import ProductsPage from './ProductsPage/ProductsPage';
 import LoginPage from "./LoginPage/LoginPage";
+import SingleProductPage from "./SingleProductPage/SingleProductPage";
 
 export {
     LandingPage,
     ProductsPage,
     LoginPage,
+    SingleProductPage,
 }
