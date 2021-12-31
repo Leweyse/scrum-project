809413c95e39c43f8c91216a4c0654dc69d80e20
Leweyse <darylcastro67@gmail.com>
2021-12-31 21:06:46 +0100

    implemented pagination

--- client/src/components/container/StockSection/StockSection.js
+++ client/src/components/container/StockSection/StockSection.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import apiClient from "../../../services/apiClient";
 
@@ -6,15 +6,29 @@ import { ProductCard, Spinner } from '../../block';
 
 const StockSection = () => {
     const [data, setData] = useState(null);
+    const pageNumber = useRef(1);
+
+    const ITEMS_PER_PAGE = 32;
+
+    const getProducts = async () => {
+        const res = await apiClient.get(`products/page/${pageNumber.current}/${ITEMS_PER_PAGE}`);
+        setData(res.data.data);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setData(null);
+
+        let value = e.target.value;
+        pageNumber.current = parseInt(value) + 1;
+
+        getProducts();
+    }
 
     useEffect(() => {
         // Cleanup issue in useEffect: https://dev.to/pallymore/clean-up-async-requests-in-useeffect-hooks-90h
         const abortController = new AbortController();
-
-        const getProducts = async () => {
-            const res = await apiClient.get('products');
-            setData(res.data.data);
-        }
 
         getProducts();
 
@@ -26,24 +40,80 @@ const StockSection = () => {
 
     return (
         <>
-            { data !== null ? 
-                <main id={'stockSection'}>
-                { data.products.map((product, idx) => {
-                    return (
-                        <ProductCard
-                            key={idx}
-                            toId={product.id}
-                            // src={product.image}
-                            src={'https://m.media-amazon.com/images/I/61kocbtP2QL._AC_SL1000_.jpg'}
-                            // title={product.title}
-                            title={'Vinyl Ex:Re'}
-                            price={`${product.price / 100} $`}
-                            seller={product.user}
-                        />
+        { data !== null ?
+            <main id={"stockSection"}>
+                <section className={"paginationContainer"}>
+                    { Array.from(Array(Math.round(data.totalLength / ITEMS_PER_PAGE))).map((page, idx) => {
+                        if ((pageNumber.current - 1) === idx) {
+                            return (
+                                <button
+                                    key={idx}
+                                    onClick={handleSubmit}
+                                    value={idx}
+                                    className={"currentPage"}
+                                >
+                                    {idx + 1}
+                                </button>
+                            )
+                        } else {
+                            return (
+                                <button
+                                    key={idx}
+                                    onClick={handleSubmit}
+                                    value={idx}
+                                >
+                                    {idx + 1}
+                                </button>
+                            )}
+                        }
                     )}
-                )}
-                </main>
-            : <Spinner /> }
+                </section>
+                <section className={'stockContainer'}>
+                    { data.products.map((product, idx) => {
+                        return (
+                            <ProductCard
+                                key={idx}
+                                toId={product.id}
+                                // src={product.image}
+                                src={'https://m.media-amazon.com/images/I/61kocbtP2QL._AC_SL1000_.jpg'}
+                                // title={product.title}
+                                title={'Vinyl Ex:Re'}
+                                price={`${(product.price / 100).toFixed(2)} $`}
+                                seller={product.user}
+                            />
+                        )}
+                    )}
+                </section>
+                <section className={"paginationContainer"}>
+                    { Array.from(Array(Math.round(data.totalLength / ITEMS_PER_PAGE))).map((page, idx) => {
+                        if ((pageNumber.current - 1) === idx) {
+                            return (
+                                <button
+                                    key={idx}
+                                    onClick={handleSubmit}
+                                    value={idx}
+                                    className={"currentPage"}
+                                >
+                                    {idx + 1}
+                                </button>
+                            )
+                        } else {
+                            return (
+                                <button
+                                    key={idx}
+                                    onClick={handleSubmit}
+                                    value={idx}
+                                >
+                                    {idx + 1}
+                                </button>
+                            )}
+                        }
+                    )}
+                </section>
+            </main>
+            :
+            <Spinner />
+        }
         </>
     )
 }
