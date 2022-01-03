698604bba4fae75da401018dd4026b1d400fdbdb
Leweyse <darylcastro67@gmail.com>
2022-01-03 16:46:46 +0100

    removed unnecessary props

--- client/src/components/container/SingleProductSection/SingleProductSection.js
+++ client/src/components/container/SingleProductSection/SingleProductSection.js
@@ -1,31 +1,66 @@
-export default function SingleProductSection (props) {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import apiClient from "../../../services/apiClient";
+
+import { Spinner } from "../../block";
+
+export default function SingleProductSection() {
+    let { id } = useParams();
+    const [data, setData] = useState(null);
+
+    const getProduct = async () => {
+        const res = await apiClient.get(`product/${id}`);
+        setData(res.data.data)
+    }
+
+    useEffect(() => {
+        const abortController = new AbortController();
+
+        getProduct();
+
+        return () => {
+            abortController.abort();
+        }
+    })
+    
     return (
-        <main id={"productPage"}>
-            <div className={"productPageLeft"}>
-                <img className={"productImage"} src={props.src} alt={"Product"}/>
-            </div>
-            <div className={"productPageRight"}>
-                <p className={"productRightElements"}>
-                    <span className={"productPageBold"}>Product: </span>
-                    {props.name}
-                </p>
-                <p className={"productRightElements"}>
-                    <span className={"productPageBold"}>Price: </span>
-                    {props.price}
-                </p>
-                <p className={"productRightElements"}>
-                    <span className={"productPageBold"}>Seller: </span>
-                    {props.seller}
-                </p>
-                <p className={"productRightElements"}>
-                    <span className={"productPageBold"}>Description: </span>
-                    {props.description}
-                </p>
-                <div className={"productRightElements productPriceStatistics"}>
-                    <p>Price stats go here.</p>
-                </div>
-                <button className={"productPageAddToCart"}>Add to Cart</button>
-            </div>
-        </main>
+        <>
+            { data !== null ?
+                <main id={"productPage"}>
+                    <div className={"productPageLeft"}>
+                        <img 
+                            className={"productImage"} 
+                            src={'https://m.media-amazon.com/images/I/61kocbtP2QL._AC_SL1000_.jpg'} 
+                            alt={"Product"}
+                        />
+                    </div>
+                    <div className={"productPageRight"}>
+                        <p className={"productRightElements"}>
+                            <span className={"productPageBold"}>Product: </span>
+                            {data.product.title}
+                        </p>
+                        <p className={"productRightElements"}>
+                            <span className={"productPageBold"}>Price: </span>
+                            {`${(data.product.price / 100).toFixed(2)} $`}
+                        </p>
+                        <p className={"productRightElements"}>
+                            <span className={"productPageBold"}>Seller: </span>
+                            {data.product.user}
+                        </p>
+                        <p className={"productRightElements"}>
+                            <span className={"productPageBold"}>Description: </span>
+                            {data.product.description}
+                        </p>
+                        <div className={"productRightElements productPriceStatistics"}>
+                            <p>Price stats go here.</p>
+                        </div>
+                        <button className={"productPageAddToCart"}>Add to Cart</button>
+                    </div>
+                </main>
+            : 
+                <Spinner/> 
+            }
+        </>
     )
 }
--- client/src/pages/SingleProductPage/SingleProductPage.js
+++ client/src/pages/SingleProductPage/SingleProductPage.js
@@ -1,43 +1,10 @@
 import { Footer, Navbar, SingleProductSection } from "../../components";
-import {useParams} from "react-router-dom";
-import apiClient from "../../services/apiClient";
-import {useEffect, useState} from "react";
-import {Spinner} from "../../components/block";
 
 export default function SingleProductPage () {
-
-    const [data, setData] = useState(null);
-    let {id} = useParams()
-
-    const getProduct = async () => {
-        const res = await apiClient.get(`product/${id}`);
-        setData(res.data.data)
-        console.log(res.data.data);
-    }
-
-    useEffect(() => {
-
-        const abortController = new AbortController();
-
-        getProduct();
-
-        return () => {
-            abortController.abort();
-        }
-
-    }, [])
-
     return (
         <>
             <Navbar/>
-            {data !== null ?
-                <SingleProductSection
-                src={'https://m.media-amazon.com/images/I/61kocbtP2QL._AC_SL1000_.jpg'}
-                name={data.product.title}
-                price={data.product.price / 100}
-                seller={data.product.user}
-                description={data.product.description}
-            /> : <Spinner/> }
+            <SingleProductSection />
             <Footer />
         </>
     )
