25fe22b28dabee83e4dc02a921065a30bcd27747
jvondzerza <jvondzerza@googlemail.com>
2022-01-04 15:49:42 +0100

    minor formatting

--- api/app/Http/Controllers/Test/FakeStatController.php
+++ api/app/Http/Controllers/Test/FakeStatController.php
@@ -10,20 +10,21 @@ use App\Models\Stat;
 class FakeStatController extends Controller
 {
     public function fake() {
-        for($i=0;$i<100;$i++) {
-            $product_id = rand(1,500);
+            for($i=0;$i<100;$i++) {
+                $product_id = rand(1,500);
 
-            if($product = Product::find($product_id)) {
-                $new_price = rand($product->price - 40,$product->price + 100);
-                Stat::create([
-                    'products_id' => $product_id,
-                    'old_price' => $product->price,
-                    'new_price' => $new_price
-                ]);
-                $product->price = $new_price;
-                $product->save;
+                if($product = Product::find($product_id)) {
+                    $new_price = rand($product->price - 40,$product->price + 100);
+                    Stat::create([
+                        'products_id' => $product_id,
+                        'old_price' => $product->price,
+                        'new_price' => $new_price
+                    ]);
+
+                    $product->price = $new_price;
+                    $product->save();
+                }
+
             }
-
         }
-    }
 }
