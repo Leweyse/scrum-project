f15663018dc8ad3a090c49f52689119b7291ebb2
Leweyse <darylcastro67@gmail.com>
2022-01-06 11:56:36 +0100

    removed extra auth

--- client/src/pages/EditListingPage/EditListingPage.js
+++ client/src/pages/EditListingPage/EditListingPage.js
@@ -1,97 +1,52 @@
+import { useContext } from 'react';
+
+import { Context } from '../../services/Context';
+
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from "react-router-dom";
-import useCookie, { getCookie } from 'react-use-cookie';
 import { ListingSection, Navbar, Footer } from "../../components";
 import { Spinner } from '../../components/block';
 import apiClient from '../../services/apiClient';
 
 export default function EditListingPage () {
-    let navigate = useNavigate();
+    const props = useContext(Context);
+
     let { id } = useParams();
+    let navigate = useNavigate();
+
+    const [user, setUser] = useState(null);
     const [product, setProduct] = useState(null);
-    const [userId, setUserId] = useState(null);
-    const [tkn, setTkn] = useState(getCookie('token'));
-    const [userToken, setUserToken] = useCookie('token','0');
     const [isLoading, setIsLoading] = useState(true);
 
+    const getProduct = async () => {
+        const res = await apiClient.get(`product/${id}`, {
+            headers: {
+                Accept: 'application/json'
+            }
+        })
 
-    useEffect(() => {
-        authCheck();
-    });
-
-
-    // const getProduct = () => {
-    //     apiClient.get(`product/${id}`, {
-    //         headers: {
-    //             Accept: 'application/json'
-    //         }
-    //     })
-    //     .then((res) => {
-    //         if(res.data.data.product) {
-    //             setProduct(res.data.data.product);
-    //         }
-    //         else {
-    //             navigate("/products");
-    //         }
-            
-    //     });
-    // }
-
-    const authCheck = () => {
-        if(tkn && tkn !== '0') {
-            setUserToken(tkn);
-            apiClient.get('user', {
-                headers: {
-                    Accept: 'application/json',
-                    Authorization: `Bearer ${userToken}`,
-                }
-            })
-                .then((res) => {
-                    apiClient.get(`product/${id}`, {
-                        headers: {
-                            Accept: 'application/json'
-                        }
-                    })
-                    .then((resp) => {
-                        if(resp.data.data.product) {
-                            if(res.data.data.user.id !== resp.data.data.product.users_id) {
-                                navigate("/profile");
-                            }
-                            else{
-                                setUserId(res.data.data.user.id);
-                                setProduct(resp.data.data.product);
-                                setIsLoading(false);
-                            }
-                        }
-                        else {
-                            navigate("/products");
-                        }
-                        
-                    });
-                })
-                .catch((err) => {
-                    if (err.response && err.response.status === 401) {
-                        setUserId(null);
-                        setTkn('0');
-                        setUserToken('0');
-                        navigate("/login");
-                    }
-                });
-        }
-        else {
-            setUserId(null);
-            setTkn('0');
-            setUserToken('0');
-            navigate("/login");
+        if (res.data.data.product) {
+            if (props.user.id !== res.data.data.product.users_id) {
+                navigate("/profile");
+            } else {
+                setUser(props.user.id);
+                setProduct(res.data.data.product);
+                setIsLoading(false);
+            }
+        } else {
+            navigate("/products");
         }
     }
 
+    useEffect(() => {
+        getProduct();
+    });
 
     return (
         <>
             <Navbar/>
             { !isLoading ? 
-                <ListingSection type={"update"} userId={userId} product={product}/>
+                <ListingSection type={"update"} user={user} product={product}/>
             : 
                 <Spinner /> 
             }
